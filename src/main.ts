e6b09d03dd996952ba097e5b56fdc6b6532c988d
Aman <emantweb@gmail.com>
2024-11-16 14:34:25 +0300

    feat: version api, and dot handling added

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors();
   app.enableVersioning();
   app.setGlobalPrefix("api");
+  await app.listen(3000);
 }
 bootstrap();
